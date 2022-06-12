6d58150e0c7ec9216b712d6d47f54e7d7b2d2778
kmj <mjoo1106@naver.com>
2022-06-13 00:09:43 +0900

    Modify company analysis page

--- visual/server.js
+++ visual/server.js
@@ -116,7 +116,72 @@ app.get('/analysis', (req, res) => {
                 "ans":Number(user_rows[i][6].slice(0,4))})
     }
     apt.push(Math.round((usum/total)*100))
+
+    const fs2 = require("fs");
+    const path2=require("path");
+    const csvPath2=path2.join(__dirname,'analysis_data','algo_correct_stat_rankgroup.csv')
+    const csv2=fs2.readFileSync(csvPath2,"utf-8")
+    const rows2=csv2.split("\n");
+    const user_rows2=[]
+    var usum2=0;
+    var total2=0;
+    for(var i=0; i<rows2.length; i++){
+        var temp2=rows2[i].split(",")
+        if(temp2[0]=="골드 3"){
+            user_rows2.push(temp2)
+            total2+=Number(temp2[2])
+            usum2+=Number(temp2[3])
+        }
+    }
+    apt.push(Math.round((usum2/total2)*100))
+
+    user_rows.sort(function(a,b){
+        return Number(b[6])-Number(a[6])
+    });
+    for(var i=0; i<10; i++){
+        apt.push({"type":user_rows[i][2],
+                "ans":Number(user_rows[i][6].slice(0,4))})
+    }
+    user_rows2.sort(function(a,b){
+        return Number(b[2])-Number(a[2])
+    });
+    for(var i=0; i<10; i++){
+        apt.push({"type":user_rows2[i][1],
+                "try":user_rows2[i][2],
+                "ans":Number(user_rows2[i][5].slice(0,4))})
+    }
+
+
     res.send(apt);
+});
+app.get('/company_analysis', (req, res) => {
+    // user|line_score1|line_score2|line_inferior|line_superior|line_acceptance_probability|
+    // kakao_score1|kakao_score2|kakao_inferior|kakao_superior|kakao_acceptance_probability|
+    // coupang_score1|coupang_score2|coupang_inferior|coupang_superior|coupang_acceptance_probability|
+    // samsung_score1|samsung_score2|samsung_inferior|samsung_superior|samsung_acceptance_probability|
+    // naver_score1|naver_score2|naver_inferior|naver_superior|naver_acceptance_probability
+    const fs = require("fs");
+    const path=require("path");
+    const csvPath=path.join(__dirname,'analysis_data','user_with_acceptance_prediction.csv')
+    const csv=fs.readFileSync(csvPath,"utf-8")
+    const rows=csv.split("\n");
+    for(var i=0; i<rows.length; i++){
+        var temp=rows[i].split(",")
+        if(temp[0]=="mjoo1106"){
+            break;
+        }
+    }
+    const arg=[];
+    for(i=5; i<=25; i+=5){
+        if(temp[i].indexOf("E")!=-1){
+            arg.push(0);
+        }
+        else{
+            arg.push(Math.round(Number(temp[i])*100))
+        }
+    }
+    console.log(arg);
+    res.send(arg);
 });
 
 app.listen(8000, () => {
